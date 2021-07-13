da03ea9dc338f95205464ec6a91cc42cec83a6a4
Minh Dinh Nguyen <minh.nguyen2@hitachivantara.com>
2021-07-13 18:44:55 +0700

    in progress

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -5,20 +5,22 @@ import { useHistory } from 'react-router-dom';
 import FirebaseContext from '../../context/firebase';
 
 function Login(props) {
-    const history = useHistory();
-    const { firebase } = useContext(FirebaseContext);
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
 
-    const [emailAddress, setEmailAddress] = useState('')
-    const [password, setPassword] = useState('')
+  const [emailAddress, setEmailAddress] = useState('')
+  const [password, setPassword] = useState('')
 
-    const [error, setError] = useState('')
-    const [isInvalid, setIsInvalid] = useState(false)
+  const [error, setError] = useState('')
+  const [isInvalid, setIsInvalid] = useState(false)
 
-    return (
-        <p>
-          I'm the login page  
-        </p>
-    )
+  return (
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <div className="flex w-3/5">
+        <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instagram app" />
+      </div>
+    </div>
+  )
 }
 
 Login.propTypes = {
