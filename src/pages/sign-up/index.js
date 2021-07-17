36d2d1c4ad0fabb089081ee5197ab605872a3619
Minh Dinh Nguyen <minhnd0496@gmail.com>
2021-07-17 23:56:55 +0700

    add sign-up/index.js

---
+++ src/pages/sign-up/index.js
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import PropTypes from 'prop-types';
+// import { connect } from 'react-redux';
+
+export const SignUp = props => <div>SignUp</div>
+
+SignUp.propTypes = {
+//   props: PropTypes
+};
+
+const mapStateToProps = state => ({
+
+})
+
+const mapDispatchToProps = {
+
+};
+
+// export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
+export default SignUp;
