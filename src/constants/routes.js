23f89cfbc4f8fc5fc6e8496daa5437f9cef81eba
Minh Dinh Nguyen <minhnd0496@gmail.com>
2021-07-18 01:10:33 +0700

    in progress

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 export const DASHBOARD = '/'
-export const LOGIN = '/'
-export const SIGN_UP = '/'
+export const LOGIN = '/login'
+export const SIGN_UP = '/sign-up'
 export const PROFILE = '/'
 export const NOT_FOUND = '/'
